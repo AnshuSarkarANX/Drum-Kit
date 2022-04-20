81280eaf23d52f047423d16377bb3cead829e94a
AnshuSarkarANX <anshusarkaranx@gmail.com>
2022-04-20 16:35:08 +0530

    changed

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
  var numberofbtns = document.querySelectorAll('.drum').length;
-  var tom_audio = new Audio("sounds/tom-1.mp3");
  for (var i = 0; i < numberofbtns; i++) {
-  document.querySelectorAll('.drum')[i].addEventListener('click',tom);
- }
- function tom(){
+   document.querySelectorAll('.drum')[i].addEventListener('click',soundss);}
+   function soundss() {
+    var btntxt = this.textContent;
+    switch (btntxt) {
+      case "w":
+        var crash = new Audio('sounds/crash.mp3');
+        crash.play();
+        break;
+      case "a":
+        var kick = new Audio('sounds/kick-bass.mp3');
+         kick.play();
+      break;
+      case "s":
+        var snare = new Audio('sounds/snare.mp3');
+        snare.play();
+      break;
+      case "d":
+        var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+      break;
+      case "j":
+        var tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+      break;
+      case "k":
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+      break;
+      case "l":
+        var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+      break;
+      default:
+        console.log(buttonhtml);
 
- tom_audio.play();
+    }
 
- }
+  }
